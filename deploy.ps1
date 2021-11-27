d6fc1ac48365e535f9d3a0fb1b41dd37e9d043da
Gary J <github@accu.ca>
2021-11-27 10:57:03 -0800

    Add File

---
+++ deploy.ps1
@@ -0,0 +1,45 @@
+# Load the System.Windows.Forms assembly into PowerShell
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create a new Form object and assign to the variable $Form
+$Form = New-Object System.Windows.Forms.Form
+$Form.Text = "Lighthouse Labs VM  Installer"
+
+$FontFace = New-Object System.Drawing.Font(
+  "Comic Sans MS",16,[System.Drawing.FontStyle]::Regular
+  )
+  
+# Initialize the forms font
+$Form.Font = $FontFace
+
+# Create a Label object
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "This will deploy the LHL WSL Image on your system"
+$Label.AutoSize = $true
+$Form.Controls.Add($Label)
+
+# AutoSize ensures the Form size can contain the text
+$Form.AutoSize = $true
+$Form.AutoSizeMode = "GrowOnly"
+
+
+
+
+#Initialize Form so it can be seen
+$Form.showDialog()
+
+function getImage {
+  # Download archive
+  Write-Host "Downloading Archive"
+  Invoke-WebRequest 'www.google.ca' -OutFile 'd:\tmp\test.html'
+  
+  # extract archive
+  Write-Host "Extracting Archive"
+  Expand-Archive d:\tmp\test.zip -DestinationPath d:\tmp
+
+}
+
+
+
+
+# https://www.windows10forums.com/articles/windows-powershell-tutorial-8-forms-part-1.73/
