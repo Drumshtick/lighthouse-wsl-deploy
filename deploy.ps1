ade97547f1155cdb4aac7e234ee5c16f335dcae6
Gary J <github@accu.ca>
2021-11-27 21:00:19 -0800

    Update

--- deploy.ps1
+++ deploy.ps1
@@ -39,31 +39,107 @@ $outputBox.Font = $FontFace
 
 $Form.Controls.Add($OutputBox)
 
-$Button = New-Object System.Windows.Forms.Button
-$Button.Location = New-Object System.Drawing.Size(230, 50)
-$Button.Size = New-Object System.Drawing.Size(110, 80)
-$Button.Text = "Click to Start"
-$Button.Add_Click( { OnClick } )
-$Form.Controls.Add($Button)
+$DeployButton = New-Object System.Windows.Forms.Button
+$DeployButton.Location = New-Object System.Drawing.Size(230, 50)
+$DeployButton.Size = New-Object System.Drawing.Size(110, 80)
+$DeployButton.Text = "Step 2: `r`nDownload VM Image"
+$DeployButton.Add_Click( { OnClick } )
+$Form.Controls.Add($DeployButton)
 
-$CloseButton = New-Object System.Windows.Forms.Button
-$CloseButton.Location = New-Object System.Drawing.Size(230, 50)
-$CloseButton.Size = New-Object System.Drawing.Size(110, 80)
-$CloseButton.Text = "Close"
-$CloseButton.Add_Click( { $Form.Close() } )
+$EnableButton = New-Object System.Windows.Forms.Button
+$EnableButton.Location = New-Object System.Drawing.Size(40, 50)
+$EnableButton.Size = New-Object System.Drawing.Size(110, 80)
+$EnableButton.Text = "Step 1: `r`nEnable WSL2"
+$EnableButton.Add_Click( { EnableWSL })
+$Form.Controls.Add($EnableButton)
+
+$CloseButton1 = New-Object System.Windows.Forms.Button
+$CloseButton1.Location = New-Object System.Drawing.Size(40, 50)
+$CloseButton1.Size = New-Object System.Drawing.Size(110, 80)
+$CloseButton1.Text = "Exit"
+$CloseButton1.Add_Click( { $Form.Close() } )
+
+$CloseButton2 = New-Object System.Windows.Forms.Button
+$CloseButton2.Location = New-Object System.Drawing.Size(230, 50)
+$CloseButton2.Size = New-Object System.Drawing.Size(110, 80)
+$CloseButton2.Text = "Exit"
+$CloseButton2.Add_Click( { $Form.Close() } )
 
 $started = $false;
 $ErrorActionPreference = 'Stop'
 $tarFile = "$env:temp\Lighthouse_wsl-v1.2.tar"
 
+function  EnableWSL {
+  $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
+  $error.Clear()
+  # $EnableButton.Enabled = $false
+  $DeployButton.Enabled = $false
+  $EnableButton.Text = "Running"
+
+  # outputs multi-byte char response
+  $ver = ""
+  $output1 = Invoke-Expression 'c:\windows\system32\wsl.exe --status'
+  foreach ($item in $output1) {
+    $nstr = toString($item)
+    if ($nstr.length -gt 3) {
+      # $match = ($nstr -Match "v.e.r.s.i.o.n.:.\s.5")
+      $match = ($nstr -Match "version: 5")
+      if ($match) {
+        Write-Host  $item
+        $outputBox.text += "$nstr`r`n"
+        $ver = $item
+      }
+    }
+  }
+
+  Write-Host  "WSL Enabled=$match" 
+  if ($match) {
+    $msg = "You system already has WSL enabled.  You can proceed to Step 2"
+    $outputBox.text += "$msg`r`n"
+    $DeployButton.Enabled = $true
+    return
+  }
+
+
+  $isAdmin = [Security.Principal.WindowsPrincipal]::new(
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+  ).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+  $outputBox.text += "Admin=$isAdmin`r`n`r`n"
+  if (!$isAdmin) {
+    $msg = "This Part of the process requires PowerShell to be started as Administrator`r`n`r`nPlease run PowerShell as Administrator and re-run"
+    $outputBox.text += "$msg`r`n"
+ 
+    $Form.Controls.Remove($EnableButton)
+    $Form.Controls.Add($CloseButton1)
+    return
+  }
+
+  # Download enable WSL cmd file
+
+  $outputBox.text += "Enabling WSL ...`r`n"
+  try {
+    Invoke-Item 'D:\tmp\enable.cmd'
+  }
+  catch {
+    $outputBox.text += "$out`r`n"
+  }
+
+  $outputBox.text += "Code = $LASTEXITCODE`r`n" 
+
+  if (!$error) {
+    $EnableButton.Text = "Done!"
+    $outputBox.text += " `r`nDone!"
+  }
+  $outputBox.text += "$error`r`n"
+}
 function  OnClick {
   if ($started) {
     return
   }
   $started = $true;
-  $Button.Enabled = $false
-  $Button.Text = "Running"
-
+  $DeployButton.Enabled = $false
+  $DeployButton.Text = "Running"
   Write-Host "started"
 
   $url = "https://www.dropbox.com/s/ybtx4cn1351z2pr/Lighthouse_wsl-v1.2.zip?dl=1"
@@ -86,8 +162,8 @@ function  OnClick {
   }
 
   Cleanup($ZipFile)
-  $Form.Controls.Remove($Button)
-  $Form.Controls.Add($CloseButton)
+  $Form.Controls.Remove($DeployButton)
+  $Form.Controls.Add($CloseButton2)
 }
 
 function  Download {
@@ -114,13 +190,14 @@ function  UnPack {
   Write-Host "Unpacking: $Filename"
   
   try {
-    Expand-Archive $Filename  -DestinationPath $env:temp
-    # Expand-Archive -Force $Filename
+    Expand-Archive -Force  $Filename  -DestinationPath $env:temp
   }
   catch {
     $outputBox.text += " `r`n$error"  
   }
 }
+
+
 
 
 function  Cleanup {
@@ -132,6 +209,18 @@ function  Cleanup {
 
   Write-Host "Deleting: $tarFile"
   Remove-Item $tarFile
+}
+
+function toString {
+  param ($in)
+  $out = "";
+  $chars = $in.toCharArray()
+  foreach ($char in $chars) {
+    if ($char) {
+      $out += $char
+    }
+  }
+  return $out;
 }
 
 [void] $Form.showDialog()
